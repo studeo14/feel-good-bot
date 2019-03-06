9db0de0841133fbf4da1440a7e26bdeb5b777913
Steven Freder <sfrederiksen@vt.edu>
2019-03-06 15:56:11 -0500

    Added app.py

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
-# TODO
+import os
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from tinydb import TinyDB, Query
+from tinydb.operations import increment
+from flask import Flask, request
+from utils import get_members, reply
+
+app = Flask(__name__)
+
+bot_id = os.environ['BOT_ID']
+access_token = os.environ['ACCESS_TOKEN']
+group_id = os.environ['GROUP_ID']
+
+BOT_NAME = '@FeelGoodBot'
+
+def is_request(message):
+    return BOT_NAME in message['text']
+
+def read_from_db():
+    db = TinyDB('./db.json')
+    leaders = db.all()
+    return leaders
+
+def ids_to_names(leaders):
+    members = get_members(group_id, access_token)
+    mim = {member['user_id']: member['nickname'] for member in members}
+    return [{**leader, 'name': mim[leader['uid']]} for leader in leaders]
+
+def send_leaderboard(message):
+    recip = message['name']
+    leaders = read_from_db()
+    leaders = ids_to_names(leaders)
+    leaders_stats_string = ''
+    for leader in leaders:
+        leaders_stats_string += '{}: {}'.format(leader['name'], leader['stars'])
+    message = "@{}\nAll time stats:\n{}".format(recip, leaders_stats_string)
+    reply(message, bot_id)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    message = request.get_json()
+    if is_request(message):
+        send_leaderboard(message)
---
+++ run_app.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ./real_envs.sh
+FLASK_APP=app.py
+python3 -m flash run
